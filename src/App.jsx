ff67577b6c1eab6d2c2cf04f8ae948823db8a34f
EldaJashari <eldajashari5@outlook.com>
2025-05-01 18:29:34 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ const App = () => {
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
+      <Footer/>
     </div>
   )
 }
